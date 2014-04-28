06f8f3b5e4af92605bfb8b94cd3ca849a2ef1128
mmercedes <matthewmercedes@gmail.com>
2014-04-28 16:06:15 -0400

    created cache library

--- pcache.c
+++ pcache.c
@@ -1,1 +1,62 @@
+#include <stdlib.c>
+#include <string.h>
 #include "csapp.h"
+
+cache* cache_new(){
+	c = malloc(sizeof(cache));
+	c->start = NULL;
+	c->end = NULL;
+	c->size = 0;
+	return c;
+}
+
+void cache_free(cache* c){
+	object* current = c->start;
+	object* next = NULL;
+
+	while(current != NULL){
+		next = current->next;
+		free(current->key);
+		free(current->data);
+		free(current);
+		current = next;
+	}
+	free(c);
+	return;
+}
+
+void cache_add(cache* c, char* key, char* data, size_t size){
+	object* obj = malloc(sizeof(object));
+	obj->key = malloc(strlen(key)*sizeof(char));
+	obj->data = malloc(size*sizeof(char));
+
+	strcpy(obj->key, key);
+	strcpy(obj->data, data);
+
+	obj->next = c->start;
+	obj->prev = NULL;
+	c->start->prev = obj;
+	c->start = obj;
+	c->size += size;
+	return;
+}
+
+void cache_update(cache* c, object* obj){
+	if(obj->next != NULL) obj->next->prev = obj->prev;
+	if(obj->prev != NULL) obj->prev->next = obj->next;
+	obj->next = c->start;
+	obj->prev = NULL;
+	c->start->prev = obj;
+	c->start = obj;
+	return;
+}
+
+object* cache_lookup(cache* c, char* key){
+	current = c->start;
+
+	while(current != NULL){
+		if(!strcmp(current->key, key)) return current;
+		current = current->next;
+	}
+	return NULL;
+}
--- pcache.h
+++ pcache.h
@@ -1,20 +1,26 @@
-#ifndef __PCACHE_H__
-#define __PCACHE_H__
+#ifndef PCACHE_H_
+#define PCACHE_H_
 
-#define MODE "LRU"
+typedef struct object
+{
+	char* key;
+	char* data;
+	struct object* next;
+	struct object* prev;
+} object;
 
+typedef struct cache
+{
+	struct object* start;
+	struct object* end;
+	size_t size;
+} cache;
 
-
-
-
-
-
-
-
-
-
-
-
+cache* cache_new();
+void cache_free(cache* c);
+void cache_add(cache* c, char* key, char* data, size_t size);
+void cache_update(cache* c, object* obj);
+object* cache_lookup(cache* c, char* key);
 
 
 #endif
